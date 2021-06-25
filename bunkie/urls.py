2579fe0b7a15d8a169c12b77482b405d434d91da
Joseph Chinedu <josephchinedu34@gmail.com>
2021-06-25 10:44:13 +0100

    implemented comment section

--- bunkie/urls.py
+++ bunkie/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import *
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
-    path('filter/', room_filter, name='filter')
+    path('filter/', room_filter, name='filter'),
+    path('comments/', CommentList.as_view()),
+    path('comments/<int:pk>/', CommentDetail.as_view()),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
